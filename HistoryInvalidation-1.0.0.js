97bb290c1b5354a38c4b32c55a19b09c004d80cd
Alex Fischer <alex@quadrant.net>
2020-10-14 11:06:53 -0600

    Prevent 'double reload' issue

--- HistoryInvalidation-1.0.0.js
+++ HistoryInvalidation-1.0.0.js
@@ -4,17 +4,28 @@
 */
 (function() {
     'use strict';
-    if (!history.state || !history.state.back_reload_loaded) {
+    function is_definitely_reloading() {
+        try {
+            return performance.getEntriesByType('navigation')[0].type == 'reload';
+        } catch (e) {
+            // If the browser doesn't support Performance API, we can't say for sure that the page is reloading
+            return false
+        }
+    }
+    if (
+        !history.state || !history.state.HistoryInvalidation_loaded // This page was just navigated to. Assume it's fresh. You need to set caching headers such that pages aren't normally cached
+        || is_definitely_reloading()
+    ) {
         set_load_time();
     }
     function set_load_time() {
         var state = history.state || {};
-        state.back_reload_loaded = Date.now();
+        state.HistoryInvalidation_loaded = Date.now();
         history.replaceState(state, document.title, location.href);
     }
     function reload_if_stale() {
-        if (!localStorage.back_reload_invalidated) return
-        if (localStorage.back_reload_invalidated < history.state.back_reload_loaded) return
+        if (!localStorage.HistoryInvalidation_invalidated) return
+        if (localStorage.HistoryInvalidation_invalidated < history.state.HistoryInvalidation_loaded) return
         set_load_time();
         location.reload();
     }
@@ -29,7 +40,7 @@
     */
     window.HistoryInvalidation = {
         invalidate: function() {
-            localStorage.back_reload_invalidated = Date.now();
+            localStorage.HistoryInvalidation_invalidated = Date.now();
         },
     };
 })();
